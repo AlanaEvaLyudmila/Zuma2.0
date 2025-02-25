35ddfbf8f2b96f04c566bced7f9155a7ff7cf383
stud <stud@09-D1-84>
2025-02-25 12:05:55 +0300

    Добавьте файлы проекта.

---
+++ Zuma2.0/MainForm.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Zuma2._0
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelBalls = new System.Windows.Forms.Panel();
+            this.labelColorBall = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // panelBalls
+            // 
+            this.panelBalls.Location = new System.Drawing.Point(41, 45);
+            this.panelBalls.Name = "panelBalls";
+            this.panelBalls.Size = new System.Drawing.Size(627, 310);
+            this.panelBalls.TabIndex = 0;
+            this.panelBalls.Paint += new System.Windows.Forms.PaintEventHandler(this.panelBalls_Paint);
+            this.panelBalls.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panelBalls_MouseClick);
+            // 
+            // labelColorBall
+            // 
+            this.labelColorBall.AutoSize = true;
+            this.labelColorBall.Location = new System.Drawing.Point(317, 388);
+            this.labelColorBall.Name = "labelColorBall";
+            this.labelColorBall.Size = new System.Drawing.Size(57, 20);
+            this.labelColorBall.TabIndex = 1;
+            this.labelColorBall.Text = "Шарик";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelColorBall);
+            this.Controls.Add(this.panelBalls);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelBalls;
+        private System.Windows.Forms.Label labelColorBall;
+    }
+}
+
---
+++ Zuma2.0/MainForm.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zuma2._0
+{
+    public partial class MainForm : Form
+    {
+        List <Color> ballColor = new List <Color> ();
+        Color CurrentBall;
+        int sizeBall = 40;
+        int trackBall = 10;
+        Random random = new Random ();
+        public MainForm()
+        {
+            InitializeComponent();
+            for (int i = 0; i < trackBall; i++)
+            {
+                ballColor.Add(GetRandomColor());
+            }
+            NewBall();
+        }
+
+        void RemoveBalls()
+        {
+            for (int i = 0; i < ballColor.Count - 2 ; i++)
+            {
+                if(ballColor[i]==ballColor[i+1]&&ballColor[i]==ballColor[i+2])
+                {
+                    ballColor.RemoveRange(i, 3);
+                    i = Math.Max(i - 3, -1);
+                }
+            }
+        }
+        void NewBall()
+        {
+            CurrentBall = GetRandomColor();
+            labelColorBall.BackColor = CurrentBall;
+
+        }
+
+        Color GetRandomColor()
+        {
+            Color [] Palette = {Color.Red, Color.Green, Color.Blue, Color.Yellow};
+            return Palette[random.Next(Palette.Length)];   
+        }
+        private void panelBalls_Paint(object sender, PaintEventArgs e)
+        {
+            for (int i = 0; i < ballColor.Count; i++)
+            {
+                var brush = new SolidBrush(ballColor[i]);
+                e.Graphics.FillEllipse(brush, i * sizeBall, 0, sizeBall, sizeBall);
+            }
+        }
+
+        private void panelBalls_MouseClick(object sender, MouseEventArgs e)
+        {
+            int InsertIndex = e.X/ sizeBall;
+            if(InsertIndex >= 0 && InsertIndex<=ballColor.Count )
+            {
+                ballColor.Insert(InsertIndex,CurrentBall);
+                panelBalls.Invalidate();
+                RemoveBalls();
+                NewBall();
+            }
+        }
+
+    }
+}
